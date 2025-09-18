91db3f6abd1f6957131b878e7069000c6043f85f
Jungwoo4021 <96278935+Jungwoo4021@users.noreply.github.com>
2025-09-18 23:04:39 +0900

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -3,61 +3,74 @@
 import torch
 import torch.nn as nn
 
+from experiments.eval_only.test_code.models.svmixer import SVMixer
+from experiments.eval_only.test_code.models.ecapa import ECAPA_TDNN
+
 # 필수: 이 레포에서 제공하는 엔트리포인트 함수 목록
 dependencies = ["torch", "torchvision"]
 
-def small_svmixer(pretrained=False, **kwargs):
-    from experiments.eval_only.test_code.models.svmixer import SVMixer
+class ModelWrapper(nn.Module):
+    def __init__(self, sv_mixer, classifier) -> None:
+        super().__init__()
 
-    model = SVMixer(5, 149, 1024)
+        self.sv_mixer = sv_mixer
+        self.classifier = classifier
+
+    def forward(self, x):
+        x = self.sv_mixer(x)
+        x = self.classifier(x)
+        return x
+
+def small(pretrained=False, **kwargs):
+    """
+    5 layers
+    Size: 33.0M parameters
+    GMACs: 11.9
+    Vox1-O EER: 0.91%
+    """
+    sv_mixer = SVMixer(5, 149, 1024)
+    ecapa = ECAPA_TDNN(5, 1024, 512, 192)
 
     if pretrained:
         # 사전 학습된 가중치를 불러오기 (예: 릴리스에 올려둔 파일에서 다운로드)
-        ckpt = torch.hub.load_state_dict_from_url(
+        sv_mixer_ckpt = torch.hub.load_state_dict_from_url(
             "https://github.com/Jungwoo4021/SV-Mixer/raw/main/assets/trained_models/svmixer_5layer_eer0.91_student_model.pt",
             map_location="cpu"
         )
-        model.load_state_dict(ckpt)
-    return model
-
-def large_svmixer(pretrained=False, **kwargs):
-    from experiments.eval_only.test_code.models.svmixer import SVMixer
-
-    model = SVMixer(17, 149, 1024)
-
-    if pretrained:
-        # 사전 학습된 가중치를 불러오기 (예: 릴리스에 올려둔 파일에서 다운로드)
-        ckpt = torch.hub.load_state_dict_from_url(
-            "https://github.com/Jungwoo4021/SV-Mixer/raw/main/assets/trained_models/svmixer_17layer_eer0.78_student_model.pt",
-            map_location="cpu"
-        )
-        model.load_state_dict(ckpt)
-    return model
-
-def small_classifier(pretrained=False, **kwargs):
-    from experiments.eval_only.test_code.models.ecapa import ECAPA_TDNN
-
-    model = ECAPA_TDNN(5, 1024, 512, 192)
-
-    if pretrained:
-        # 사전 학습된 가중치를 불러오기 (예: 릴리스에 올려둔 파일에서 다운로드)
-        ckpt = torch.hub.load_state_dict_from_url(
+        ecapa_ckpt = torch.hub.load_state_dict_from_url(
             "https://github.com/Jungwoo4021/SV-Mixer/raw/main/assets/trained_models/svmixer_5layer_eer0.91_classifier.pt",
             map_location="cpu"
         )
-        model.load_state_dict(ckpt)
+        sv_mixer.load_state_dict(sv_mixer_ckpt)
+        ecapa.load_state_dict(ecapa_ckpt)
+
+    model = ModelWrapper(sv_mixer, ecapa)
+    
     return model
 
-def large_classifier(pretrained=False, **kwargs):
-    from experiments.eval_only.test_code.models.ecapa import ECAPA_TDNN
-
-    model = ECAPA_TDNN(17, 1024, 512, 192)
+def large(pretrained=False, **kwargs):
+    """
+    17 layers
+    Size: 80.0M
+    GMACs: 19.4
+    Vox1-O EER: 0.78%
+    """
+    sv_mixer = SVMixer(17, 149, 1024)
+    ecapa = ECAPA_TDNN(17, 1024, 512, 192)
 
     if pretrained:
         # 사전 학습된 가중치를 불러오기 (예: 릴리스에 올려둔 파일에서 다운로드)
-        ckpt = torch.hub.load_state_dict_from_url(
+        sv_mixer_ckpt = torch.hub.load_state_dict_from_url(
+            "https://github.com/Jungwoo4021/SV-Mixer/raw/main/assets/trained_models/svmixer_17layer_eer0.78_student_model.pt",
+            map_location="cpu"
+        )
+        ecapa_ckpt = torch.hub.load_state_dict_from_url(
             "https://github.com/Jungwoo4021/SV-Mixer/raw/main/assets/trained_models/svmixer_17layer_eer0.78_classifier.pt",
             map_location="cpu"
         )
-        model.load_state_dict(ckpt)
+        sv_mixer.load_state_dict(sv_mixer_ckpt)
+        ecapa.load_state_dict(ecapa_ckpt)
+
+    model = ModelWrapper(sv_mixer, ecapa)
+    
     return model
